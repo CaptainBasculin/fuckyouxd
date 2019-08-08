7097e6a48329cc46762a62cc4f404d51ea2d480e
collabvm-users <53918988+collabvm-users@users.noreply.github.com>
2019-08-08 17:15:17 -0400

    Our first commit
    Nice

---
+++ main_code.py
@@ -0,0 +1,10 @@
+# collabvm-user:0w0whatsThis
+import os;
+
+if 1 == 2:
+    print("LOOK AT THIS DUDE")
+
+print("this is a test")
+for i in reversed(range(0, 50)):
+    print(i, "seconds until forking")
+    os.w
